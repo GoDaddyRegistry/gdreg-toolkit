5d02d7bbe663fb3bf54fd2eaa620f70d83aa66d9
shivam.choudhary <shivam.choudhary@IMP-ITPL0368.impetus.co.in>
2022-02-01 18:48:05 +0530

    MM-18447 : Move CN and TW Extensions from registrar to GD Toolkit.

---
+++ src/main/java/godaddy/registry/jtoolkit2/se/cnnic/CNNICExtensionConstants.java
@@ -0,0 +1,19 @@
+package godaddy.registry.jtoolkit2.se.cnnic;
+
+/**
+ * CNNIC Extension Constants.
+ */
+public final class CNNICExtensionConstants {
+
+    /** The Constant CNNIC_CONTACT_REGISTRANT_EXTENSION. */
+    public static final String CNNIC_CONTACT_REGISTRANT_EXTENSION = "cnnic-contact";
+
+    /** The Constant CNNIC_CONTACT_REGISTRANT_EXTENSION_URI. */
+    public static final String CNNIC_CONTACT_REGISTRANT_EXTENSION_URI = "urn:ietf:params:xml:ns:cnnic-contact-1.0";
+
+    /**
+     * Instantiates a new CNNIC extension constants.
+     */
+    private CNNICExtensionConstants() {
+    }
+}
